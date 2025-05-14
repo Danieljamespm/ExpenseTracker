7f618ae7acd1dcbc0cf52ce3af2aa42dbf22d4ab
Danny Mettler <danieljamespm@gmail.com>
2025-05-13 22:36:15 -0400

    fixed typo causing expenses page to crash.

--- client/src/components/Charts/RecentExpensesWithChart.jsx
+++ client/src/components/Charts/RecentExpensesWithChart.jsx
@@ -17,7 +17,7 @@ const RecentExpensesWithChart = ({data, totalExpenses}) => {
     
         return Object.entries(grouped).map(([category, total]) => ({
           name: category,
-          amount: Number(total,tofixed(2)),
+          amount: Number(total,toFixed(2)),
         }))
       }
     
