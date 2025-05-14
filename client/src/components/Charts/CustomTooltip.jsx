e154dd9b3130c7652d5f3819a4b5984d1880380f
Danny Mettler <danieljamespm@gmail.com>
2025-05-13 22:09:34 -0400

    having issues with the tooltil showing the background text of pie chart through the tool tip.

--- client/src/components/Charts/CustomTooltip.jsx
+++ client/src/components/Charts/CustomTooltip.jsx
@@ -4,10 +4,10 @@ const CustomTooltip = ({active, payload}) => {
     if(active && payload && payload.length) {
         const value = Number(payload[0].value.toFixed(2))
         return (
-            <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
-                <p className="text-xs font-semibold text-[var(--color-primary)] mb-1">{payload[0].name}</p>
-                <p className="text-sm text-gray-600">
-                    Amount: {' '} <span className='text-sm font-medium text-gray-900'>${value}</span>
+            <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300 opactiy-100">
+                <p className="text-xs font-semibold text-[var(--color-primary)] mb-1 opactiy-100">{payload[0].name}</p>
+                <p className="text-sm text-gray-600 opacity-100">
+                    Amount: {' '} <span className='text-sm font-medium text-gray-900 opacity-100'>${value}</span>
                 </p>
             </div>
         )
